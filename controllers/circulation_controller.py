e917ca279dba3759a66c33fc8d06f2188b866218
KhanhNguyen171 <nguyenvukhanh171@gmail.com>
2026-01-23 11:51:08 +0700

    style: updated circulation_view logic code, update circulation_controller view_fine_detail, pay_fine, process returning. update circulation get/update fine_detail, get member_type/dua_date

--- controllers/circulation_controller.py
+++ controllers/circulation_controller.py
@@ -7,7 +7,7 @@ class CirculationController:
         self.model = CirculationModel()
         self.daily_fine_rate = 5000
 
-    def process_borrowing(self, member_id, book_id, member_type, manual_due_date=None):
+    def process_borrowing(self, member_id, book_id, manual_due_date=None):
         """
         Xử lý yêu cầu mượn sách
         manual_due_date: Ngày do Librarian chọn thủ công (nếu có không thì mặc định sau 14 ngày)
@@ -26,6 +26,10 @@ class CirculationController:
             return False, "System Error: Could not retrieve member stats."
         borrowed_count, total_fines = member_stats
         
+        # Kiểm tra giới hạn mượn ở student và teacher
+        member_type = self.model.get_member_type(member_id)
+        if not member_type:
+            return False, "Error: Member ID not found."
         # giới hạn mượn 5 với student và 10 với teacher
         limit = 10 if member_type == 'Teacher' else 5
         
@@ -53,8 +57,8 @@ class CirculationController:
             # Default: 14 ngày kể từ hôm nay
             due_date = datetime.now().date() + timedelta(days=14)
         
-        # Tạo Transaction theo định dạng ISS-datetime thêm microseconds để tránh trùng lặp
-        trans_id = f"ISS-{datetime.now().strftime('%Y%m%d%H%M%S%f')}"
+        # Tạo Transaction theo định dạng T-datetime thêm microseconds để tránh trùng lặp
+        trans_id = f"T-{datetime.now().strftime('%Y%m%d%H%M%S%f')}"
         success = self.model.create_issue_transaction(trans_id, member_id, book_id, due_date)
         
         if success:
@@ -62,7 +66,7 @@ class CirculationController:
         else:
             return False, "System Error: Failed to record transaction."
 
-    def process_returning(self, book_id, due_date):
+    def process_returning(self, book_id):
         """
         Xử lý yêu cầu trả sách và tự động tính tiền phạt, 
         trả về thành công hoặc thất bại và giá tiền phạt,
@@ -70,24 +74,26 @@ class CirculationController:
         VD: ngày mượn 2026-01-17  --> 2026-01-31
         --> tính phạt sau ngày 31, ép về date = Y-M-D để xử lý không dùng H:M:S
         """
+        # Lấy due_date từ db thông qua book_id
+        raw_due_date = self.model.get_due_date(book_id)
+        
+        if not raw_due_date:
+            # Trả tuple cố định để view không bị unpack lỗi
+            return False, 0, 0, "Error: No active borrow transaction found for this book."
+        
+        # Ép kiểu về .date() để chỉ so sánh YYYY-MM-DD
+        if isinstance(raw_due_date, datetime):
+            due_date = raw_due_date.date() 
+        else:
+            due_date= raw_due_date
+        
+        
         # Lấy thời gian hệ thống hiện tại 
         return_date = datetime.now().date()
-        
-        # Chuyển due_date nếu đang string sang datetime
-        if isinstance(due_date, str):
-            try:
-                due_date = datetime.strptime(due_date, "%Y-%m-%d").date()
-            except ValueError:
-                return False, "Invalid date format. Please use YYYY-MM-DD."
-        elif isinstance(due_date, datetime):
-            due_date = due_date.date()
-        elif isinstance(due_date, date):
-            pass
-        else:
-            return False, "Invalid due_date type."
             
         # Automated Fine Calculation
         fine_amount = 0
+        overdue_delta = timedelta(0)
         if return_date > due_date:
             overdue_delta = return_date - due_date
             # Chỉ tính tiền phạt nếu số ngày quá hạn >= 1
@@ -98,6 +104,34 @@ class CirculationController:
         success = self.model.update_return_book(book_id, fine_amount)
         
         if success:
-            return True, fine_amount
+            return True, fine_amount, overdue_delta.days, "Book returned successfully."
+
+        return False, fine_amount, overdue_delta.days, "System Error: Failed to update return transaction."
+    
+    def view_fine_details(self, trans_id):
+        """Xử lý logic hiển thị chi tiết tiền phạt"""
+        fine = self.model.get_fine_details(trans_id)
         
-        return False, fine_amount
+        if not fine:
+            return False, "Fine record not found for this Transaction ID."
+        
+        # Giải nén dữ liệu từ tuple (transID, amount, isPaid, lateDays)
+        t_id, amount, is_paid, late_days = fine
+        
+        status = "Paid" if is_paid else "Unpaid"
+        
+        # Trả về kết quả đã format
+        details = (
+            f"Transaction ID: {t_id}\n"
+            f"Late days: {late_days}\n"
+            f"Fine amount: {amount:,.0f} VND\n"
+            f"Payment status: {status}"
+        )
+        return True, details
+
+    def pay_fine(self, trans_id):
+        """Xử lý logic thanh toán tiền phạt"""
+        success = self.model.update_fine_payment(trans_id)
+        if success:
+            return True, "Fine paid successfully."
+        return False, "Failed to update payment status."
